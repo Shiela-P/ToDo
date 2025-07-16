3ad7b4118d10cb5f7edb7bd85d25221926bfdda1
Shiela-P <125091812+Shiela-P@users.noreply.github.com>
2025-07-15 22:34:24 -0400

    Linked Javascript file and tested to make sure is functional

---
+++ script.js
@@ -0,0 +1,9 @@
+// This is the button used to add a task to the list
+const addButton = document.getElementById("add_button");
+
+// Function to add task to task list
+const addTask = () => {
+    alert("test");
+};
+
+addButton.addEventListener("click", addTask)
